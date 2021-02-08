9d8e647839c1f8eac1b4aa02063b49090a7e3955
KarthigaVijaykumar <karthigamadhu8599@gmail.com>
2021-02-08 09:39:25 +0530

    First commit in master branch

---
+++ src/PrintListOFNames.java
@@ -0,0 +1,10 @@
+
+public class PrintListOFNames {
+	public static void main(String[] args) {
+		String names [] = {"aalia","shajin","padmini","huzaima"};
+		System.out.println("printing list of names");
+		for(String name : names) {
+			System.out.println(name);
+		}
+	}
+}
---
+++ src/PrintListOfFruits.java
@@ -0,0 +1,10 @@
+
+public class PrintListOfFruits {
+	public static void main(String[] args) {
+		String names [] = {"aalia","shajin","padmini","huzaima"};
+		System.out.println("printing list of names");
+		for(String name : names) {
+			System.out.println(name);
+		}
+	}
+}
